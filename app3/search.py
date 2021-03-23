2058e9e46af5a37d54714c798fcb853cd72eca6e
sherry242 <71978502+sherry242@users.noreply.github.com>
2021-03-23 20:22:53 +0000

    Add files via upload

---
+++ app3/search.py
@@ -0,0 +1,48 @@
+import functools
+from flask import Flask, render_template
+import json
+import os
+
+
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+bp = Blueprint('search', __name__, url_prefix='/')
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def search():
+	Result = ''
+	choice = ''
+	if request.method == 'POST':
+		content = request.form['search']
+		choice = request.values.get("search_method")
+		Result = "search method: "+choice+"\nsearch content: "+content
+		# print(Result)
+		error = None
+
+		if not content:
+			error = 'Content Cannot be None.'
+
+		flash(error)
+
+
+	#数组传参
+	#num=["","",""]
+
+	#字典传参
+	num = {
+			1: ['10001', '123', 'Mike', '2017-5-6'],
+			2: ['10002', '123', 'Sam', '2018-5-6'],
+			3: ['10003', '123', 'hong', '2019-5-6']
+		}
+
+	return render_template('music.html', num=num)
+
+def algorithm(content):
+	return content
+
+
+
